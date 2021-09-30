bb2550c33f69187d2419da3c2367bde856200082
riky08 <reekesh08@gmail.com>
2021-09-30 17:56:24 -0500

    added nwy file

---
+++ string_boolean_if_else.java
@@ -0,0 +1,24 @@
+
+public class string_boolean_if_else {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		String name = "Rikesh Shrestha";
+//		System.out.println("My name is " + name + ".");
+		
+//		int x = 10;
+//		System.out.println( x != 10);
+		
+		int grade = 30;
+		
+		if(grade>40) {
+		System.out.println("PASS");
+		}
+		else {
+		System.out.println("FAIL");
+		}
+		
+		
+	}
+
+}
